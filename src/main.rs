bd08f5b5947f76377a389a214af9512567681d6b
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2025-10-18 23:13:45 +0530

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
  fn main() {
-    let x = 1;
-    println!("The value of x is: {}", x);
+    let x: i32 = 1;
     println!("Hello, world!");
     let mut result: i32 = sum(127, 1);
+    println!("The value of x is: {}", x);
     let z: f32 = 2.0;
+    println!("The value of result is: {}", result);
     println!("The value of z is: {}", z);
     println!("The sum of 127 and 1 is: {}", result);
+
+    let greeting: String = String::from("Hello, Rust!");
+    println!("{}", greeting);
+
+    let isEven: bool = true;
+    if isEven {
+        println!("The number is even.");
+    } else {
+        println!("The number is odd.");
+    }
+
+    for i in 1..5 {
+        println!("Iteration number: {}", i);
+    }
+
+    // Iteration of arrays, maps, strings
+
+
+}
+
+fn get_first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+    return &s[..];
 }
 
 fn sum(a: i32, b: i32) -> i32 {
